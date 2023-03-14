5438ef0d54f207965891a4c8574a75adf788dfa5
prashanth reddy <prashanthrdd9@gmail.com>
2023-03-14 15:25:20 -0400

    initial commit

--- DemoSakara/ViewController.swift
+++ DemoSakara/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let viewModel = ViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        viewModel.delegate = self
+        
+        viewModel.processSwapAPIRequest()
+        
+        let nib = UINib(nibName: "ShipViewCell", bundle: nil)
+        
+        tableView.register(nib, forCellReuseIdentifier: "shipcellidentifier")
     }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.shipsModel?.results.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "shipcellidentifier", for: indexPath) as? ShipViewCell else {
+            return UITableViewCell()
+        }
+        let shipData = viewModel.shipsModel?.results[indexPath.row]
+        cell.configureView(shipData?.name ?? "No information")
+        return cell
+    }
+}
 
+extension ViewController: ViewModelDelegate {
+    func populateListView() {
+        DispatchQueue.main.async {[weak self] in
+            self?.tableView.reloadData()
+        }
+    }
 }
 
